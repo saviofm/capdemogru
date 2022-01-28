dbdff6a76f484fa9f341b4d6d9378adf5ef018b2
Savio Marques <savio.marques@sap.com>
2022-01-28 12:35:27 +0000

    Add pre-parcel APP

---
+++ app/preparcel/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("capdemogru.preparcel.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
